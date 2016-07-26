b4a0d728481426dc886b955c7809455ddce71743
Brendan Matkin <brendan@tangibleinteraction.com>
2016-07-25 18:02:50 -0700

    In progress library conversion (non-functional). Started header file with probable requirements, created a nearly empty cpp file.

---
+++ client/client_generic/TCT.cpp
@@ -0,0 +1,11 @@
+
+#include "Arduino.h"
+#include "TCT.h"
+
+// probably do the OSC in here!  --  does this mean I need UDP in here too? 
+#include <OSCMessage.h>
+#include <OSCBundle.h>
+
+TCT::TCT(module_type_t moduleType) {
+
+}
---
+++ client/client_generic/TCT.h
@@ -0,0 +1,46 @@
+#ifndef _TCT_h_
+#define _TCT_h_
+
+#include "Arduino.h"
+#include <WiFiUDP.h>
+
+typedef enum {
+  INPUT_MODULE
+  OUTPUT_MODULE
+  SNIFFER_MODULE
+  TRANSLATOR_MODULE
+} module_type_t;
+
+typedef enum {
+  CONNECTED
+  UPDATING
+  UPDATE_SUCCESSFUL
+  UPDATE_FAILED
+} status_led_t;
+
+class TCT {
+  public: 
+    TCT(module_type_t mT);
+
+    void sendData();					// send input data
+    void sendInfo();					// send module information: name, battery voltage, module type, mode, FW version, etc.
+    void checkMode();  					// check DIP switches
+    void readIncoming();				// parse incoming OSC messages (maybe return something?)
+
+    bool pairMode(uint8_t timeout);   	// returns true when paired
+
+    void statusLED(status_led_t lS);	// make the light do the things!
+    void requestOTA();					// request OTA update from server (/check for new version)
+
+    uint8_t beginMulticast(IPAddress _interface, IPAddress _multi, uint16_t _port);		// pass through to WiFiUDP
+
+  private:
+  	module_type_t moduleType;
+  	void checkConfig();
+
+  	virtual int beginPacketMulticast(IPAddress _multi, uint16_t _port, IPAddress _interface, int ttl = 1);  // pass WiFiUDP
+  	virtual int endPacket();			// pass WiFiUDP
+};
+
+#endif 
+/* _TCT_h_ */
