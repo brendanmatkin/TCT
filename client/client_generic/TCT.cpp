f0ab09470f1f1ae6801e4baeb742d678dd867dc0
Brendan Matkin <brendan@tangibleinteraction.com>
2016-07-25 18:45:08 -0700

    library conversion - save progress..

--- client/client_generic/TCT.cpp
+++ client/client_generic/TCT.cpp
@@ -5,7 +5,29 @@
 // probably do the OSC in here!  --  does this mean I need UDP in here too? 
 #include <OSCMessage.h>
 #include <OSCBundle.h>
+#include <WiFiUdp.h>
 
 TCT::TCT(module_type_t moduleType) {
+	_moduleType = moduleType;
+}
+
+void TCT::sendData() {
+	// send sensor data over OSC
+}
+
+void TCT::sendInfo() {
+	// send device info over OSC
+}
+
+void TCT::checkMode() {
+	// check DIP switches 
+}
+
+bool TCT::pairMode(bool buttonPress, uint8_t timeout) {
+	return paired;
+}
+
+void TCT::statusLED(status_led_t sL) {
 
 }
+
--- client/client_generic/TCT.h
+++ client/client_generic/TCT.h
@@ -4,6 +4,8 @@
 #include "Arduino.h"
 #include <WiFiUDP.h>
 
+
+
 typedef enum {
   INPUT_MODULE
   OUTPUT_MODULE
@@ -22,21 +24,24 @@ class TCT {
   public: 
     TCT(module_type_t mT);
 
-    void sendData();					// send input data
-    void sendInfo();					// send module information: name, battery voltage, module type, mode, FW version, etc.
-    void checkMode();  					// check DIP switches
-    void readIncoming();				// parse incoming OSC messages (maybe return something?)
+    void sendData(float _a, float _b);  // send input data
+    void sendInfo();                    // send module information: name, battery voltage, module type, mode, FW version, etc.
+    void checkMode();  					        // check DIP switches
+    void readIncoming();				        // parse incoming OSC messages (maybe return something?)
 
-    bool pairMode(uint8_t timeout);   	// returns true when paired
+    bool pairMode(bool buttonPress, uint8_t timeout);   	// returns true when paired
 
-    void statusLED(status_led_t lS);	// make the light do the things!
-    void requestOTA();					// request OTA update from server (/check for new version)
+    void statusLED(status_led_t sL);  	// make the light do the things!
+    void requestOTA();        					// request OTA update from server (/check for new version)
 
     uint8_t beginMulticast(IPAddress _interface, IPAddress _multi, uint16_t _port);		// pass through to WiFiUDP
 
   private:
-  	module_type_t moduleType;
+  	module_type_t _moduleType;
   	void checkConfig();
+
+    uint8_t _timeout;
+    bool _paired;
 
   	virtual int beginPacketMulticast(IPAddress _multi, uint16_t _port, IPAddress _interface, int ttl = 1);  // pass WiFiUDP
   	virtual int endPacket();			// pass WiFiUDP
